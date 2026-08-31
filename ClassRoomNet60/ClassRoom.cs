2b23afd66e3cc742a07bc194292ee55fc3a043a8
Neozt001 <neo001@edu.zealand.dk>
2026-08-31 11:21:41 +0200

    Add Task 2

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public string ClassName { get; set; }
+        public List<Student> StudentList { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+            
+        }
+    }
+}
