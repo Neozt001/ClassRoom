941a58463bd5b3483c6636e67b8f186d763ed763
Neozt001 <neo001@edu.zealand.dk>
2026-08-31 11:32:43 +0200

    Add Task 3

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 3
+using ClassRoomNet60;
+
+ClassRoom c1 = new ClassRoom();
+c1.ClassName = "3Q";
+c1.SemesterStart = new DateTime(26, 8, 2019);
+
+c1.StudentList.Add(new Student("S1", 1, 1));
+c1.StudentList.Add(new Student("S2", 2, 2));
+c1.StudentList.Add(new Student("S3", 3, 3));
