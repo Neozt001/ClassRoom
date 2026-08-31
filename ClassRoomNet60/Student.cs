9ffc9d0d833dc50e430b1b04353d61cc075eb4b1
Neozt001 <neo001@edu.zealand.dk>
2026-08-31 11:18:52 +0200

    Add Task 1

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string Name { get; }
+        public int BirthMonth { get; }
+        public int BirthDay { get; }
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthDay = birthDay;
+        }
+    }
+}
